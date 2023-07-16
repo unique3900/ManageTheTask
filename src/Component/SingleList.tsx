5de847e5da51bf573f8eb46cd25805e55a7aa41f
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-07-16 18:56:25 +0545

    Handle completed Task

--- src/Component/SingleList.tsx
+++ src/Component/SingleList.tsx
@@ -1,35 +1,68 @@
 import React from 'react'
-import { TaskModel } from '../model'
-import {AiFillEdit,AiFillDelete} from 'react-icons/ai'
-import {TiTick} from 'react-icons/ti'
-interface Props{
-  task: TaskModel;
+import {
+    TaskModel
+} from '../model'
+import {
+    AiFillEdit,
+    AiFillDelete
+} from 'react-icons/ai'
+import {
+    TiTick
+} from 'react-icons/ti'
+interface Props {
+    task: TaskModel;
     tasks: TaskModel[];
-    setTasks:React.Dispatch<React.SetStateAction<TaskModel[]>>
+    setTasks: React.Dispatch < React.SetStateAction < TaskModel[] >>
 }
-const SingleList:React.FC = ({tasks,setTasks,task}:Props) => {
-    
-    const handleDelete = (id:number) => {
-        setTasks(tasks.filter((item)=>item.id!=id))
+const SingleList: React.FC = ({
+    tasks,
+    setTasks,
+    task
+} : Props) => {
+
+    const handleDelete = (id : number) => {
+        setTasks(tasks.filter((item) => item.id != id))
     }
-    const handleDone = (id:number) => {
-        
+    const handleDone = (id : number) => {
+        setTasks(tasks.map((task) => task.id == id ? {
+            ...task,
+            status: !task.status
+        } : task))
     }
-  return (
-    <div className='shadow-md w-1/3 px-3 py-5 bg-yellow-500 text-black flex flex-row gap-3 justify-between items-center'>
-          <div className="">
-              <p className="font-bold text-xl">{task.title}</p>
-        </div>
-      
-          <div className="flex flex-row gap-4">
-              <AiFillEdit className="scale-125 cursor-pointer" />
-              <AiFillDelete onClick={() => {
-                  handleDelete(task.id)
-              }}  className="scale-125 cursor-pointer" />
-              <TiTick className="scale-150 cursor-pointer"/>
-          </div>
+    return (
+        <div className='shadow-md w-1/3 px-3 py-5 bg-yellow-500 text-black flex flex-row gap-3 justify-between items-center'>
+            <div className="">
+                <p className={
+                    task.status ? "font-bold text-xl line-through" : "font-bold text-xl"
+                }>
+                    {
+                    task.title
+                }</p>
+            </div>
+
+            <div className="flex flex-row gap-4">
+
+                {
+                !task.status && (
+                    <AiFillEdit className="scale-125 cursor-pointer"/>
+                )
+            }
+
+                <AiFillDelete onClick={
+                        () => {
+                            handleDelete(task.id)
+                        }
+                    }
+                    className="scale-125 cursor-pointer"/>
+                <TiTick onClick={
+                        () => {
+                            handleDone(task.id)
+                        }
+                    }
+                    className="scale-150 cursor-pointer"/>
+            </div>
     </div>
-  )
+    )
 }
 
 export default SingleList
