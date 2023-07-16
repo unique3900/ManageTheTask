94222841ec5ae61339ff551aae119f4a1e6876ef
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-07-16 19:18:10 +0545

    Edit Task Action Performed

--- src/Component/SingleList.tsx
+++ src/Component/SingleList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     TaskModel
 } from '../model'
@@ -20,31 +20,59 @@ const SingleList: React.FC = ({
     task
 } : Props) => {
 
+  const [editEnabled, seteditEnabled] = useState<boolean>(false)
+  const [editedText, setEditedText] = useState<string>(task.title);
     const handleDelete = (id : number) => {
         setTasks(tasks.filter((item) => item.id != id))
     }
     const handleDone = (id : number) => {
-        setTasks(tasks.map((task) => task.id == id ? {
-            ...task,
-            status: !task.status
-        } : task))
-    }
+      setTasks(tasks.map((task) => task.id == id ? {
+        ...task,
+        status: !task.status
+      } : task));
+     
+  }
+  
+  const handleEdit = (id: number) => {
+    setTasks(tasks.map((item) => item.id == id ? { ...item, title: editedText } : item))
+    seteditEnabled(false)
+  }
     return (
         <div className='shadow-md w-1/3 px-3 py-5 bg-yellow-500 text-black flex flex-row gap-3 justify-between items-center'>
-            <div className="">
-                <p className={
-                    task.status ? "font-bold text-xl line-through" : "font-bold text-xl"
-                }>
-                    {
-                    task.title
-                }</p>
+        <div className="">
+          {
+            !editEnabled ? (
+              <p className={
+                task.status ? "font-bold text-xl line-through" : "font-bold text-xl"
+            }>
+                {
+                task.title
+            }</p>
+            ) : (
+                <form onSubmit={(e) => {
+                  e.preventDefault();
+                  handleEdit(task.id);
+                }}>
+                     <input type="text" value={editedText} onChange={(e) => {
+                    setEditedText(e.target.value)
+                  }} />
+                  
+                </form>
+               
+            )
+          }
+               
             </div>
 
             <div className="flex flex-row gap-4">
 
                 {
                 !task.status && (
-                    <AiFillEdit className="scale-125 cursor-pointer"/>
+              <AiFillEdit onClick={() => {
+                seteditEnabled(true)
+              }
+              
+                    } className="scale-125 cursor-pointer"/>
                 )
             }
 
